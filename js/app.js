128e565bb3894e90fa2d1f5b981b30fc8e92e0dc
Anthony Lu <anthony.lu1115@gmail.com>
2021-09-20 21:45:08 +1000

    functioning with charts

--- js/app.js
+++ js/app.js
@@ -3,24 +3,24 @@ const headers = {
     Accept: "application/json", 
     "Content-Type": "application/json"
   }
-  const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
+  var labels = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
   ];
-  const data = {
+  var data = {
     labels: labels,
     datasets: [{
-      label: 'Happiness',
+      label: 'Happiness over time',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45],
+      data: [],
     }]
   };
-  const config = {
+  var config = {
     type: 'line',
     data: data,
     options: {}
@@ -30,6 +30,14 @@ var myChart = new Chart(
 document.getElementById('myChart'),
 config
 );
+
+
+function updateData(chart, size, newData) {
+    chart.data.labels = [...Array(size).keys()];
+    chart.data.datasets[0].data = newData;
+    chart.update();
+}
+
 let submitButton = document.getElementById('submit');
 
 submitButton.addEventListener('click', (e) => {
@@ -37,11 +45,7 @@ submitButton.addEventListener('click', (e) => {
     let answer = document.getElementById('answers').value;
     axios.post('https://sentim-api.herokuapp.com/api/v1/', JSON.stringify({ "text": answer }), {headers: headers})
     .then((response) => {
-        let sentences = response.data.sentences;
-        let sentenceScores = [];
-        for(let sentence of sentences){
-            sentenceScores.push(sentence.sentiment.polarity)
-        };
+        
         let shrek = document.getElementById("shrek");
         rating = response.data.result.polarity;
         if (rating >= 0.75){
@@ -70,6 +74,13 @@ submitButton.addEventListener('click', (e) => {
         }
         alert(rating);
         console.log(response.data.result);
+        let sentences = response.data.sentences;
+        let sentenceScores = [];
+        for(let sentence of sentences){
+            sentenceScores.push(sentence.sentiment.polarity * 100)
+        };
+        console.log(sentenceScores);
+        updateData(myChart, sentenceScores.length + 1, sentenceScores);
     })
     .catch((error) => {
 
