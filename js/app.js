fd7dd26e2a7075e34006ae90915ca2ff2333d160
Anthony Lu <anthony.lu1115@gmail.com>
2021-09-20 19:06:43 +1000

    added image functionality

--- js/app.js
+++ js/app.js
@@ -1,12 +1,44 @@
-import axios from 'axios';
-const QuickChart = require('quickchart-js');
 
-let submitButton = document.querySelector('#submit');
+const headers = {
+    Accept: "application/json", 
+    "Content-Type": "application/json"
+  }
+let submitButton = document.getElementById('submit');
 
 submitButton.addEventListener('click', (e) => {
-    axios.post('https://sentim-api.herokuapp.com/api/v1/', { "text": e.value })
+    e.preventDefault();
+    let answer = document.getElementById('answers').value;
+    axios.post('https://sentim-api.herokuapp.com/api/v1/', { "text": answer }, {headers: headers})
     .then((response) => {
-
+        let shrek = document.getElementById("shrek");
+        rating = response.data.result.polarity;
+        console.log(typeof rating);
+        if (rating >= 0.75){
+                shrek.style.backgroundImage = "url('../images/shrekvhappy.jpeg')";
+        }else if(rating >= 0.5)
+        {
+            shrek.style.backgroundImage = "url('../images/shrekhappy.jpeg')";
+        }else if(rating >= 0.25)
+        {
+            shrek.style.backgroundImage = "url('../images/shrekgood.jpeg')";
+        }else if(rating >= 0)
+        {
+            shrek.style.backgroundImage = "url('../images/shrekneutral.jpeg')";
+        }else if(rating >= -0.25)
+        {
+            shrek.style.backgroundImage = "url('../images/shreksad.jpeg')";
+        }else if(rating >= -0.5)
+        {
+            shrek.style.backgroundImage = "url('../images/shrekmad.jpeg')";
+        }else if(rating >= -0.75)
+        {
+            shrek.style.backgroundImage = "url('../images/shrekvmad.jpeg')";
+        }else
+        {
+            shrek.style.backgroundImage = "url('../images/shrekswamp.jpeg')";
+        }
+        alert(rating);
+        console.log(response.data.result);
     })
     .catch((error) => {
         
