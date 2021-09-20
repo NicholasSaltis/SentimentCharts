0aed6c4c687c3742112fcca60f265d2aad5156e5
NicholasSaltis <gcas012107@coderacademy.edu.au>
2021-09-20 19:08:14 +1000

    axios post request functionality

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-import axios from 'axios';
-const QuickChart = require('quickchart-js');
+// import axios from 'axios';
+// const QuickChart = require('quickchart-js');
 
-let submitButton = document.querySelector('#submit');
+let submitButton = document.getElementById('submit');
 
 submitButton.addEventListener('click', (e) => {
-    axios.post('https://sentim-api.herokuapp.com/api/v1/', { "text": e.value })
+    axios.post('https://sentim-api.herokuapp.com/api/v1/', { "text": "hello hello" })
     .then((response) => {
-
+        alert(response)
+        console.log(response)
     })
     .catch((error) => {
-        
+
     })
 });
